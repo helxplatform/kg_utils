da71207751f3b3547f9d000b34dc1adde9a9b5a2
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2024-09-19 13:16:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="kg_utils",
-    version="0.0.6",
+    version="0.0.8",
     author="Evan Morris",
     author_email="",
     url="https://github.com/helxplatform/kg_utils",
